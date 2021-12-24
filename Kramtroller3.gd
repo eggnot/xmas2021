727e881f3f5985aecd5e85a25aad64c965ad7da8
eggnot <mail@eggnot.com>
2021-12-24 18:06:50 +0300

    new level 3

--- Kramtroller3.gd
+++ Kramtroller3.gd
@@ -14,6 +14,7 @@ var swipe_vector :Vector2 = Vector2.ZERO
 #var backpack:BoneAttachment
 #var last_box:MeshInstance
 onready var jp := $KramJump
+onready var turns := $KramJump/turns
 onready var model_anims := $KramJump/model_animations
 onready var backpack := $KramJump/model/Armature/Skeleton/backpack
 onready var last_box := backpack.get_node("box")
@@ -21,7 +22,7 @@ onready var last_box := backpack.get_node("box")
 var msec_jump_start := 0
 var msec_jump_ended := 0
 
-var on_ground := false
+var on_platform := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -34,18 +35,23 @@ func _physics_process(delta):
 		if Input.is_action_just_pressed("ui_accept"):
 			_jump()
 		if Input.is_action_just_pressed('ui_left'):
-			_switch_track(_clamp_track(track - 1))
-		if Input.is_action_just_pressed('ui_right'):
 			_switch_track(_clamp_track(track + 1))
+		if Input.is_action_just_pressed('ui_right'):
+			_switch_track(_clamp_track(track - 1))
 	
+	#!!!
+		
+		if jp.translation.y < 0.0:
+			jp.translation.y = 0.0
+			velocity_up = 0.0
+		#		if OS.is_debug_build():
+		#			print("jump ", OS.get_ticks_msec()-msec_jump_start, "ms")
+		elif jp.translation.y > 0.0 and not on_platform:
+			jp.translation.y += velocity_up * delta
 
-	if jp.translation.y < 0.0:
-		jp.translation.y = 0.0
-		velocity_up = 0.0
-		if OS.is_debug_build():
-			print("jump ", OS.get_ticks_msec()-msec_jump_start, "ms")
-	elif jp.translation.y > 0.0:
-		jp.translation.y += velocity_up * delta
+		var new_x = clamp(jp.translation.x, -0.75, 0.75)
+		$Camera.translation.x = lerp($Camera.translation.x, new_x, .05)
+
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
@@ -78,27 +84,27 @@ func _unhandled_input(event):
 	elif event.is_action_pressed("ui_right"):
 		pass #turn_right
 		
-	elif event.is_action_pressed("ui_up") or event.is_action_pressed("ui_accept"):
-		pass #jump
+	elif event.is_action_pressed("ui_page_up"):
+		$move_and_play.stop(false)
+		$move_and_play.play("run1")
+	elif event.is_action_pressed("ui_page_down"):
+		$move_and_play.stop(false)
+		$move_and_play.play_backwards("run1")
+		
+	elif event.is_action_pressed("ui_end"):
+		$move_and_play.stop(false)
+		
 	elif event.is_action_pressed("ui_cancel"):
 		get_tree().reload_current_scene()
-	elif event.is_action_pressed("ui_page_up"):
-		#$move_and_play.play("run1")
-		model_anims["parameters/movement/blend_position"] = 1.0
-	elif event.is_action_pressed("ui_page_down"):
-		#$move_and_play.play("run1")
-		model_anims["parameters/movement/blend_position"] = 0.0
-
+	
+	elif event.is_action_pressed("ui_home"):
+		if model_anims["parameters/movement/blend_position"] > 0.5:
+			model_anims["parameters/movement/blend_position"] = 0.0
+		else:
+			model_anims["parameters/movement/blend_position"] = 1.0
 	
 	#translation.x = clamp(translation.x, -1.2, 1.2)
 
-
-func _on_box_detector_area_entered(area:Area):
-	if area.is_in_group("boxes"):
-		add_one_box()
-		area.queue_free()
-	elif area.is_in_group("obstacles"):
-		get_tree().reload_current_scene()
 
 func add_one_box():
 	var new_box = last_box.duplicate()
@@ -106,14 +112,8 @@ func add_one_box():
 	backpack.add_child(new_box)
 	last_box = new_box
 	
-func strafe_x(x:float, t:float=0.2):
-#	$Tween.interpolate_property(self, 'translation:x',
-#	 translation.x, translation.x+x, t, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
-#	$Tween.start()
-	pass
-
 func _jump():
-	if jp.translation.y > 0.00001:
+	if jp.translation.y > 0.00001 and not on_platform:
 		return #in air
 	
 	while model_anims['parameters/jump/active']:
@@ -122,8 +122,13 @@ func _jump():
 	msec_jump_start = OS.get_ticks_msec()
 	model_anims.set('parameters/jump/active', true)
 
-	velocity_up = jump_velocity
-	jp.translation.y = 0.00001
+	if on_platform:
+		#NOT WORKING NOW
+		velocity_up = jump_velocity/3
+	else:
+		velocity_up = jump_velocity
+		jp.translation.y = 0.00001
+		
 	#sounds.get_node("Jump").play()
 
 func _clamp_track(track: int) -> int:
@@ -141,30 +146,59 @@ func _switch_track(track: int):
 	assert(-1 <= track and track <= 1)
 	
 	var target_offset = track_offset * track
-	
 	$tween.stop_all()
 	$tween.interpolate_property(jp, 'translation:x',\
-		null, target_offset, 0.2, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
+		null, target_offset, 0.35, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
 	$tween.start()
+	
+	if track < self.track:
+		turns.play("turn_left")
+	else:
+		turns.play("turn_right")
 	
 	self.track = track
 	
 	#sounds.get_node("Whoosh").play()
 
 func _on_collision(area: Area):
-	if area.is_in_group('Obstacles'):
-		# TODO
+	#var parent = area.get_parent()
+
+	if area.is_in_group('Platform'):
+		on_platform = true
+	elif area.is_in_group('Obstacles'):
 		_lose()
-		return
-	if area.is_in_group('PickupBoxes'):
-		area.get_parent().get_parent().queue_free()
-		#print('Box picked up')
+	elif area.is_in_group('Finish'):
+		_win()
+	elif area.is_in_group('PickupBoxes'):
+		add_one_box()
+#		print('PickupBox')
+	
+	area.get_parent().queue_free()
+
 		#sounds.get_node('Pickup').play()
 
+func _on_collision_exited(area):
+	if area.is_in_group('Platform'):
+#		print('platform exit')
+		on_platform = false
+
+func _win():
+	controllable = false
+	$move_and_play.stop()
+	model_anims['parameters/states/current'] = 2 # 2 is for wave!
+	turns.play("cam_zoom")
+	yield(turns, "animation_finished")
+	
+	#GO TO WIN SCENE
+	
 func _lose():
 	controllable = false
-	model_anims.set('parameters/die/active', true)
 	$move_and_play.stop()
+	model_anims['parameters/states/current'] = 1 # 1 is dead!
+	turns.play("cam_zoom")
+	yield(turns, "animation_finished")
+	get_tree().reload_current_scene()
+	
 	#death_timer.start()
 	
 	#sounds.get_node('Lose').play()
